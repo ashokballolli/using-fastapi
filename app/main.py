6a214be613b87f88776df163011f21e903e8b9ae
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-07-22 18:53:39 +0200

    examples for path and query parameters

--- app/main.py
+++ app/main.py
@@ -44,6 +44,51 @@ def get_current_user():
 def get_user(user_name: str):
     return {"logged_in_user": user_name}
 
+# Predefined values
+
+from enum import Enum
+class Environments(str, Enum):
+    dev = "development"
+    prod = "production"
+
+@app.get("/deploy/{env}")
+def get_env_name(env: Environments):
+    if env == Environments.dev:
+        return {"env": Environments.dev.value, "key": "dev key"}
+    elif env == Environments.prod:
+        return {"env": Environments.prod.value, "key": "prod key"}
+    else:
+        return {"env": "should not come here"}
+
+# Path parameters containing paths¶
+@app.get("/release/{deploy_path: path}")
+def deploy_app(deploy_path: str):
+    return {"deploymentPath": deploy_path}
+
+# Query Parameters
+dummy_user_list = ['Ashok', 'Annu', 'Alok', 'Aditi', 'Shilpa', 'Arun']
+@app.get("/userList/")
+def get_user_list(skip: int, limit: int):
+    return dummy_user_list[skip: skip+limit]
+
+# Optional parameters
+@app.get("/user_list/")
+def getUserList(limit: int, skip: Optional[int] = 0):
+    print("skip: ", skip)
+    return dummy_user_list[skip : skip+limit]
+
+# Query parameter type conversion
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="debug", reload=True)
